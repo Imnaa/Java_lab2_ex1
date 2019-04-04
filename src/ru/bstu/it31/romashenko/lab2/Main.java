28211115656d0399e6ca5fc8ebff7af4a3dd605d
Nikolai Romashchenko <kolya.romashenko@yandex.ru>
2019-04-05 00:34:12 +0300

    upd log4j

--- src/ru/bstu/it31/romashenko/lab2/Main.java
+++ src/ru/bstu/it31/romashenko/lab2/Main.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 import org.apache.logging.log4j.*;
 
 /**
+ * <p>Главный класс программы.</p>
+ *
  * @author Ромащенко Н.А.
+ * @version 1.0
+ * Дата: 21.02.19
  *
- * @version 1.
- *
- * @date 21.02.19
- *
- * @class Main
- *
- * @decription: Даны три положительных числа а, Ь, с. Проверить, будут ли они сторонами треугольника. Если да, то вычислить площадь этого треугольника
+ * Класс: Main
+ * Описание: Даны три положительных числа а, Ь, с. Проверить, будут ли они сторонами треугольника. Если да, то вычислить площадь этого треугольника
  */
-
 public class Main {
+    /** */
     static final Logger Logger = LogManager.getLogger(Main.class);
 
+    /**
+     * <p>Точка входа в программу</p>
+     *
+     * @param args - аргументы для метода
+     */
     public static void main(String[] args) {
         Logger.debug("Lets go!");
+
         System.out.println("Определение площади треугольника");
         System.out.println("\t> 1. Ввести стороны треугольника с помощью клавиатуры;");
         System.out.println("\t> 2. Считать из файла значения сторон треугольника;");
@@ -30,31 +35,40 @@ public class Main {
 
         // Инициализация объекта "Сканер"
         Scanner scanner = new Scanner(System.in);
+
         int mode = scanner.nextInt();
+
         // Инициализация объекта "Треугольник"
         Treugolnik treugolnik = new Treugolnik();
+
         // Обработка режима работы
         switch (mode) {
             // Клавиатура
             case 1: {
                 Logger.info("Пользователь выбрал режим работы 'чтение с консоли'");
+
                 // Ввод сторон треугольника
                 System.out.print("a = ");
                 treugolnik.setA(scanner.nextDouble());
-                Logger.info("Пользователь ввел в a = " + treugolnik.getA());
+                Logger.debug("Пользователь ввел в a = " + treugolnik.getA());
+
                 System.out.print("b = ");
                 treugolnik.setB(scanner.nextDouble());
-                Logger.info("Пользователь ввел в b = " + treugolnik.getB());
+                Logger.debug("Пользователь ввел в b = " + treugolnik.getB());
+
                 System.out.print("c = ");
                 treugolnik.setC(scanner.nextDouble());
-                Logger.info("Пользователь ввел в c = " + treugolnik.getC());
+                Logger.debug("Пользователь ввел в c = " + treugolnik.getC());
+
                 scanner.close();
                 break;
             }
+
             // Файл
             case 2: {
                 System.out.println("Пользователь выбрал режим работы 'чтение из файла'");
                 Logger.info("Пользователь выбрал режим работы 'чтение из файла'");
+
                 // Функция для считывания из файла
                 treugolnik.getValueFromFile();
                 break;
@@ -62,6 +76,7 @@ public class Main {
             // Выход
             case 9: {
                 System.out.println("Пользователь выбрал режим работы 'выход'");
+
                 Logger.info("Пользователь выбрал режим работы 'выход'");
                 Logger.debug("Program is end.");
                 return;
@@ -69,36 +84,48 @@ public class Main {
             // Ошибка ввода
             default: {
                 System.out.println("Выбран не верный параметр, программа завершила свою работу.");
+
                 Logger.warn("Выбран не верный параметр, программа завершила свою работу.");
                 Logger.debug("Program is end.");
                 return;
             }
         }
+
         // Вычисление периметра
         double p = treugolnik.getPerimatr();
+
         if (-1 == p) {
             System.out.println("p не посчиталось вычислить");
             Logger.error("p не посчиталось вычислить");
+
             // Обработка ошибки
             Logger.debug("Program is end.");
             return;
         }
         Logger.info("Вычислили p = " + p);
         System.out.println(" > P = " + p);
+
         treugolnik.setP(p);
+
         // Вычисление площади
         double s = treugolnik.getPloshad();
+
         if (-1 == s) {
             System.out.println("s не посчиталось вычислить");
             Logger.error("s не посчиталось вычислить");
+
             // Обработка ошибки
             Logger.debug("Program is end.");
             return;
         }
+
         Logger.info("Вычислили s = " + s);
         treugolnik.setS(s);
+
         System.out.println(" > S = " + s);
+
         treugolnik.printValueInFile();
+
         Logger.debug("Program is end.");
     }
 }
--- src/ru/bstu/it31/romashenko/lab2/Treugolnik.java
+++ src/ru/bstu/it31/romashenko/lab2/Treugolnik.java
@@ -1,108 +1,210 @@
 package ru.bstu.it31.romashenko.lab2;
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.String;
-import org.apache.logging.log4j.*;
 
-/** @author Ромащенко Н.А.
- *
- * @version 1. 21.02.19
- *
- * Имя класса: Treugolnik
- *
- * Назначение: для обработки операций, связанных с треугольником.
- */
+import javafx.application.Application;
+import org.apache.logging.log4j.*;
+import sun.rmi.runtime.Log;
 
 import static java.lang.Math.sqrt;
 
+/**
+ * <p>Класс для работы с треугольником</p>
+ *
+ * @author Ромащенко Н.А.
+ * @version 1.0
+ * Дата: 21.02.19
+ * Имя класса: Treugolnik
+ * Назначение: для обработки операций, связанных с треугольником.
+ */
 public class Treugolnik {
+    /**  */
     static final Logger Logger = LogManager.getLogger(Treugolnik.class);
-
+    /**
+     * Переменная для 1й стороны треугольника
+     */
     private double a;
+    /**
+     * Переменная для 2й стороны треугольника
+     */
     private double b;
+    /**
+     * Переменная для 3й стороны треугольника
+     */
     private double c;
+    /**
+     * Переменная для периметра треугольника
+     */
     private double p;
+    /**
+     * Переменная для площади треугольника
+     */
     private double s;
 
+    /**
+     * Метод для получения значения переменной a
+     *
+     * @return возващает значение переменной a
+     */
     public double getA() {
         return a;
     }
 
+    /**
+     * Метод для получения значения переменной b
+     *
+     * @return возващает значение переменной b
+     */
     public double getB() {
         return b;
     }
 
+    /**
+     * Метод для получения значения переменной c
+     *
+     * @return возващает значение переменной c
+     */
     public double getC() {
         return c;
     }
 
+    /**
+     * Метод для получения значения переменной p
+     *
+     * @return возващает значение переменной p
+     */
     public double getP() {
         return p;
     }
 
+    /**
+     * Метод для получения значения переменной s
+     *
+     * @return возващает значение переменной s
+     */
     public double getS() {
         return s;
     }
 
+    /**
+     * Метод для установки нового значения для переменной a
+     *
+     * @param a 1ая сторона треугольника
+     */
     public void setA(double a) {
         this.a = a;
     }
 
+    /**
+     * Метод для установки нового значения для переменной b
+     *
+     * @param b 2ая сторона треугольника
+     */
     public void setB(double b) {
         this.b = b;
     }
 
+    /**
+     * Метод для установки нового значения для переменной c
+     *
+     * @param c 3ая сторона треугольника
+     */
     public void setC(double c) {
         this.c = c;
     }
 
+    /**
+     * Метод для установки нового значения для переменной p
+     *
+     * @param p периметр треугольника
+     */
     public void setP(double p) {
         this.p = p;
     }
 
+    /**
+     * Метод для установки нового значения для переменной s
+     *
+     * @param s площадь треугольника
+     */
     public void setS(double s) {
         this.s = s;
     }
 
+    /**
+     * Конструктор класса
+     */
     public Treugolnik() {
-        Logger.info("Был создан объект 'Treugolnik' с помощью конструктора 'Treugolnik()'");
+        Logger.debug("Был создан объект 'Treugolnik' с помощью конструктора 'Treugolnik()'");
         this.a = -1;
         this.b = -1;
         this.c = -1;
+
+        Logger.debug("a = " + a);
+        Logger.debug("b = " + b);
+        Logger.debug("c = " + c);
+
     }
 
+    /**
+     * Конструктор класса
+     *
+     * @param a 1ая сторона треугольника
+     * @param b 2ая сторона треугольника
+     * @param c 3ая сторона треугольника
+     */
     public Treugolnik(double a, double b, double c) {
         Logger.info("Был создан объект 'Treugolnik' с помощью конструктора 'Treugolnik(double a, double b, double c)'");
         this.a = a;
         this.b = b;
         this.c = c;
+
+        Logger.debug("a = " + a);
+        Logger.debug("b = " + b);
+        Logger.debug("c = " + c);
     }
 
-    // Метод для вычисления периметра
+    /**
+     * Метод для вычисления периметра
+     *
+     * @return возращает периметр треугольника
+     */
     public double getPerimatr() {
         if (!check()) {
             Logger.warn("Периметр не прошел проверку.");
             return -1;
         }
-        Logger.info("Успешно посчитали периметр");
+
+        Logger.debug("Успешно посчитали периметр");
         return (this.a + this.b + this.c) / 2;
     }
 
-    // Метод для вычисления площади треугольника
+    /**
+     * Метод для вычисления площади треугольника
+     *
+     * @return возвращает площадь треугольника
+     */
     public double getPloshad() {
         if (!check()) {
             Logger.warn("Площадь не прошел проверку.");
             return -1;
         }
+
         Logger.info("Успешно посчитали площадь");
+
+        Logger.debug("s = " + sqrt(this.p * (this.p - this.a) * (this.p - this.b) * (this.p - this.c)));
+
         return sqrt(this.p * (this.p - this.a) * (this.p - this.b) * (this.p - this.c));
     }
 
-    // Метод для проверки на правильность треугольника
+    /**
+     * Метод для проверки на правильность треугольника
+     *
+     * @return возвращает результат функции
+     */
     private boolean check() {
         if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
             Logger.warn("Сторона(-ы) не могут быть <= 0. Следовательно, треугольник невозможен.");
@@ -115,26 +217,40 @@ public class Treugolnik {
         return true;
     }
 
-    // Считываение сторон треугольника
+    /**
+     * Считываение сторон треугольника
+     */
     public void getValueFromFile() {
-        Logger.info("Начало чтения из файла.");
+        Logger.debug("Начало чтения из файла.");
+
         try (FileReader reader = new FileReader("ex1.txt")) {
             BufferedReader r = new BufferedReader(reader);
+
             String temp = r.readLine();
             this.a = Double.parseDouble(temp);
+            Logger.debug("a = " + a);
+
             temp = r.readLine();
             this.b = Double.parseDouble(temp);
+            Logger.debug("b = " + b);
+
             temp = r.readLine();
             this.c = Double.parseDouble(temp);
+            Logger.debug("c = " + c);
 
+            Logger.debug("Конец чтения из файла.");
         } catch (IOException ex) {
-            Logger.fatal("Файл не найден. ", ex);
+            Logger.fatal("Файл не найден. Err:", ex);
+            ex.printStackTrace();
         }
     }
 
-    // Считываение сторон треугольника
+    /**
+     * Вывод сторон треугольника
+     */
     public void printValueInFile() {
         Logger.info("Начало записи в файл.");
+
         try (FileWriter writer = new FileWriter("ex1_otvet.txt", false)) {
             String text = ""
                     + "a = " + a + '\n'
@@ -143,9 +259,17 @@ public class Treugolnik {
                     + "p = " + p + '\n'
                     + "s = " + s + '\n';
             writer.write(text);
+
+            Logger.debug("a = " + a);
+            Logger.debug("b = " + b);
+            Logger.debug("c = " + c);
+            Logger.debug("p = " + p);
+            Logger.debug("s = " + s);
+
             Logger.info("В файл записали.");
         } catch (IOException ex) {
             Logger.fatal("В файл не записали. ", ex);
+            ex.printStackTrace();
         }
     }
 }
